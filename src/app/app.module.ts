49cdce1d78d22c24e52e7c19139831bd07d0a37e
Sulan Ádám <sulan.adam19@gmail.com>
2024-01-08 11:50:45 +0100

    ongoing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireDatabaseModule,FormsModule, NgbModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BaseService } from '../base.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  messages:any
+  newMessage:any
+  userName=""
 
-  constructor() {}
+  constructor(private base:BaseService) {
+    this.base.getMessages().snapshotChanges().pipe(
+      map((ch)=>ch.map(
+        (c)=>({key:c.payload.key,...c.payload.val()})
+      ))
+    ).subscribe((res)=>this.messages=res)
+  }
+  addMessage(){
+    let body={user:"Lajos:", time:new Date().toLocaleTimeString(),content:this.newMessage}
+    this.userName=body.user
+    this.base.addMessage(body)
+    this.newMessage=""
+  }
 
+  deleteMessage(body:any){
+    this.base.deleteMessage(body)
+  }
+  editMessage(body:any){
+    this.base.uploadMessage(body)
+  }
 }
